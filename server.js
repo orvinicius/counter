a4b45d86876408e7beb28dfb27212669999776ec
Vinicius Rodrigues <viniciusoliveira311@gmail.com>
2022-10-14 14:24:27 -0300

    created file public to show css and js resources on the page

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express'); //chama o express - que cria o servidor
 
 const app = express(); //instancia o express
 
+
 // CONFIGURAÇÃO DO SERVIDOR EXPRESS ----------
 
 app.set("view engine", "ejs"); //indica para o express que está utilizando o EJS
-
+app.set('views', './views');
+app.use(express.static('./public')); //instancia os arquivos css e js para rodar no servidor criado
 app.get("/", function (req, res) {//cria uma rota
-    
+    res.render("index")
 
 }) 
 
